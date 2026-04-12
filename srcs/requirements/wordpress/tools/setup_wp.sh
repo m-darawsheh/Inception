5eb7a73c71ae8bd2f6f1c9c3f239574c8c6f6739
Mohammed Darawsheh <mdarawsh@c2r2s4.42amman.com>
2026-04-12 18:49:30 +0300

    work on wordpress container

---
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl -O https://wordpress.org/latest.tar.gz
+
+tar -xvf latest.tar.gz
+
+mv wordpress/* /var/www/html/
+
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+sed -i "s/database_name_here/$MYSQL_DATABASE/" /var/www/html/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/" /var/www/html/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/" /var/www/html/wp-config.php
+sed -i "s/localhost/mariadb/" /var/www/html/wp-config.php
+
+exec php-fpm7.4 -F
