3555527a69aceb6d23d497cc86782d1c28b09a84
Mohammed Darawsheh <mdarawsh@c2r6s2.42amman.com>
2026-04-18 17:05:36 +0300

    add some dep

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,11 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
-mysqld_safe &
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-sleep 5
+    mysqld_safe --skip-networking &
 
+    while ! mysqladmin ping --silent; do
+        sleep 1
+    done
 
-mysql -u root << EOF
+    mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
@@ -13,6 +16,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+    mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+fi
 
 exec mysqld
--- srcs/requirements/wordpress/tools/setup_wp.sh
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-curl -O https://wordpress.org/latest.tar.gz
+if [ ! -f /var/www/html/wp-config.php ]; then
+    curl -O https://wordpress.org/latest.tar.gz
+    tar -xvf latest.tar.gz
+    mv wordpress/* /var/www/html/
 
-tar -xvf latest.tar.gz
+    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-mv wordpress/* /var/www/html/
+    sed -i "s/database_name_here/$MYSQL_DATABASE/" /var/www/html/wp-config.php
+    sed -i "s/username_here/$MYSQL_USER/" /var/www/html/wp-config.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/" /var/www/html/wp-config.php
+    sed -i "s/localhost/mariadb/" /var/www/html/wp-config.php
 
-cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-sed -i "s/database_name_here/$MYSQL_DATABASE/" /var/www/html/wp-config.php
-sed -i "s/username_here/$MYSQL_USER/" /var/www/html/wp-config.php
-sed -i "s/password_here/$MYSQL_PASSWORD/" /var/www/html/wp-config.php
-sed -i "s/localhost/mariadb/" /var/www/html/wp-config.php
+    chown -R www-data:www-data /var/www/html
+fi
 
 exec php-fpm7.4 -F
